7d5140b52199449b8eec53eee8d810e37b43912f
LozaLozaLoza <152804914+LozaLozaLoza@users.noreply.github.com>
2024-01-21 14:30:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,49 @@ var cv = can.getContext("2d");
 
 function empezar() {
 
+    dibujar();
+
     resize();
     window.addEventListener('resize', resize, false);
 }
 
-function klik() {
-    window.open("C:/000/programacion/JavaScript/colores/imagen%20automara/index.html", "_self");
+var margenA = 100;
+var margenB = 100;
+
+function dibujar() {
+    cv.fillStyle = "#000000";
+    cv.fillRect(margenA, margenB, 1000 - 2 * margenA, 1000 / 3 - 2 * margenB);
+    cv.fillRect(margenA, margenB + 1000 / 3, 1000 - 2 * margenA, 1000 / 3 - 2 * margenB);
+    cv.fillRect(margenA, margenB + 2 * 1000 / 3, 1000 - 2 * margenA, 1000 / 3 - 2 * margenB);
+    cv.font = "50px Arial";
+    cv.fillStyle = "#ffff00";
+    cv.textAlign = "center";
+    cv.fillText("Patrones de camuflaje", 500, margenB + 75);
+    cv.fillText("Rayas dinamicas", 500, margenB + 1000 / 3 + 75);
+    cv.fillText("Automata celular", 500, margenB + 2 * 1000 / 3 + 75);
+}
+
+function klik(event) {
+    var ratx, raty;
+    if (window.innerWidth < window.innerHeight) {
+        var t = (window.innerHeight - window.innerWidth - 0) / 2;
+        ratx = event.clientX * 1000 / window.innerWidth;
+        raty = 1000 * (event.clientY - t) / (window.innerHeight - 2 * t);
+    } else {
+        var t = (window.innerWidth - window.innerHeight - 0) / 2;
+        raty = event.clientY * 1000 / window.innerHeight;
+        ratx = 1000 * (event.clientX - t) / (window.innerWidth - 2 * t);
+    }
+    console.log(raty);
+    if (raty < 330) {
+        window.open("camuflaje/index.html", "_self");
+    } else {
+        if (raty < 660) {
+            window.open("rayas/index.html", "_self");
+        } else {
+            window.open("automata/index.html", "_self");
+        }
+    }
 }
 
 function resize() {
